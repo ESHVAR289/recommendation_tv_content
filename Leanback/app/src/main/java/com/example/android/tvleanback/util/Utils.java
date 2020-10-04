33ecd1bef7f22747c0f975c663df7cc9ba6b106e
eshvar289 <eshvar89@hotmail.com>
2020-10-04 12:01:38 +0530

    - moved Utils.java into the util package

--- Leanback/app/src/main/java/com/example/android/tvleanback/util/Utils.java
+++ Leanback/app/src/main/java/com/example/android/tvleanback/util/Utils.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.tvleanback;
+package com.example.android.tvleanback.util;
 
 import android.app.Activity;
 import android.content.Context;
